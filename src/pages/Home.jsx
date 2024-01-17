133d1b344452923b77282c014f8f41bab6f6f404
Gamze Şirin <gamze1sirin@gmail.com>
2024-01-17 14:00:41 +0300

    update home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import About from '../components/about/About'
-import Sidebar from '../components/sidebar/sidebar'
+import Sidebar from '../components/sidebar/Sidebar'
 
 const Home = () => {
 	return (
