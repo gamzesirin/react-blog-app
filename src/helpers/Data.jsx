b61bb020b1fbc42e703309793894d8376ef979f8
Gamze Şirin <gamze1sirin@gmail.com>
2024-01-17 13:54:20 +0300

    helpers folder added.

---
+++ src/helpers/Data.jsx
@@ -0,0 +1,36 @@
+const BlogData = [
+	{
+		id: '0',
+		title: 'İyi bir mühendisin 10 özelliği',
+		content:
+			'Bir senedir müdürlük yaptığım mühendis geçen ay başka bir takıma geçiş yaptı. Birebir toplantılarımızdan birinde ona son zamanlarda projesini iyi yönettiğini söyledim. O da bana kendi kendini yönetme konusunda bir aydınlanma yaşadığını, çalıştığı projelerde başarıya ulaşmak için yaptığı işin her safhasını sahiplenmeye karar verdiğini, yalnızca kod yazmakla yetinmemeye başladığını söyledi. Bir yandan onun bu bakış açısından etkilendim, onun adına sevindim, bir yandan da kendime kızdım — uzun süredir genç mühendislerin yazılım işini öğrenebilmeleri için sahiplenmenin (ownership) en önemli şey olduğunu düşünüyorum fakat altımda çalışan mühendisin bu gerçeği kendine kendine, zor yoldan öğrenmesine sebep oldum.',
+		date: '2023-08-01'
+	},
+	{
+		id: '1',
+		title: 'Şans ve çalışkanlık',
+		content:
+			'Şunu farkettim; yapılacak iş çok, işini kaliteli yapan insana inanılmaz derecede ihtiyaç var. Fakat insanlar bunu kendi lehine kullanmıyor. İş başvurularını değerlendiriyorsun, başvuranların özgemişini, LinkedIn profilini inceliyorsun, hiçbir sinyal alamıyorsun. Yüzde doksan beşten fazlasını mülakata bile çağırmıyorsun. Aralarında potansiyeli yüksek o kadar kişi var ki.. Ön elemeden geçenleri mülakat yapıyorsun. Adayın gözünün içine bakıyorsun güzel cevaplar ver de seni işe alalım, beraber birşeyler yapalım diye. Çocuk susmuş, başarılarını doğru düzgün anlatmıyor, kendini pazarlamıyor ya da pazarlayamıyor. Halbuki olay tamamen sinyal almak üzerine kurulu.',
+		date: '2023-08-02'
+	},
+	{
+		id: '2',
+		title: 'Güzel soru sorma sanatı',
+		content: `Öğrenmek güzel sorular sormakla başlıyor, cevabı kavrayabilmekle devam ediyor. Güzel soru sormak içinse problemin ne olduğunu anlamak lazım.
+			Youtube’da henüz 35 bin civari abone olmasına rağmen farklı platformlardan günde toplam 40‘a yakın mesaj ve youtube üzerinden onlarca yorum geliyor. İlginiz için hepinize teşekkürler, fakat problem şu; gelen her soruyu okuyup cevaplamak için min 4 dakika ayırsam, ki bazen on, on beş dakikayı buluyor, her gün üç saatten fazla vaktı buna harcamam gerekiyor. İnsanlara yardımcı olmak için sarfettiğim efor benim için full-time bir iş değil, gündelik sorumluluklarım devam ediyor. Hobi şeklinde sürdürdüğüm bu girişimde bir şekilde her sorusu olana yardımcı olmak istiyorum, fakat bunun için yardımınıza ihtiyacım var :)`,
+		date: '2023-08-03'
+	},
+	{
+		id: '3',
+		title: 'Konfor alanı',
+		content: `Hayat iki sekilde gecebiliyor;
+		daha guzel bir hayata sahip olmam icin ne yapmam gerekiyor diye sorgulayip, hareket halinde veya
+		sorgulamayip, dusunmeyip, gunumu kurtarayim derdindeyken vakti eriterek.
+		Hic birsey yapmamak, bilincinde olup yapmamaktan daha kotu geliyor bana. Bilincinde olan adam icin bir secim soz konusu, isin farkinda. Fakat diyor ki; benim elimden bu gelmez, keyfimi bozmayayim. Guzel, cunku arkadas kendini taniyor, potansiyelinin farkinda. Diyebilirsiniz ki kendini kucuk gorup, basaramayacagini dusunmenin neresi guzel? Bende derim ki sorunuzun cikis noktasi yanlis, nereden biliyorsunuz sorgulamayan bireyin kendine daha cok guvendigini? O ayri bir tartisma konusu.
+		
+		Biri demiski “life begins at the edge of your comfort zone” yani “hayat konfor alaninin bittigi noktada basliyor.”`,
+		date: '2023-08-04'
+	}
+]
+
+export default BlogData
