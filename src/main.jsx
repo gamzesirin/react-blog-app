716c981f7ed35a67518a64eaf19da912fe44cf59
Gamze Şirin <gamze1sirin@gmail.com>
2024-01-17 13:54:36 +0300

    added.

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
+)
